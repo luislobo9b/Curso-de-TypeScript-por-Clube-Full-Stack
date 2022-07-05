51c726fb84ede1859e961e4f6c99839c3fdfd82e
Luis Lobo <luis.lobo9b@gmail.com>
2022-07-05 01:08:55 -0300

    aula 04. tipagem explícita em primitivos

---
+++ src/04. tipagem explícita em primitivos/aula-04.ts
@@ -0,0 +1,15 @@
+console.log('aula 04');
+
+let name: string = 'Luis Lobo';
+let age: number = 29;
+let isAdmin: boolean = true;
+
+let value: number|string = '01010010';
+value = 21010010;
+
+let value2:boolean; // não defini nada por enquanto
+value2 = true;
+
+console.log(name, age, isAdmin, value, value2);
+
+export {} // https://bobbyhadz.com/blog/typescript-cannot-redeclare-block-scoped-variable

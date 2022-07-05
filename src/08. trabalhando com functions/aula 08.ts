8387821dcc8161a900521f70d73e6209395b43e6
Luis Lobo <luis.lobo9b@gmail.com>
2022-07-05 06:20:22 -0300

    08. trabalhando com functions

---
+++ src/08. trabalhando com functions/aula 08.ts
@@ -0,0 +1,14 @@
+console.log('aula 08');
+
+function person_(name: string, age: number): string{
+    let str = `Meu nome é ${name} e minha idade é ${age} anos`;
+    console.log(str);
+    return str;
+};
+person_('Luis', 29);
+
+function sum_(num1: number, num2: number): number{
+    console.log(num1 + num2);
+    return num1 + num2;
+};
+sum_(1, 2);
